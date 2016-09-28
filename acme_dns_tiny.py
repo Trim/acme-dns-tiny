8d8133429e76e3e9a2757b08b3020000f15e7852
Adrien Dorsaz <adrien@adorsaz.ch>
2016-09-28 23:30:53 +0200

    Let json to read and decode responses.

--- acme_dns_tiny.py
+++ acme_dns_tiny.py
@@ -64,7 +64,7 @@ def get_crt(config, log=LOGGER):
 
     log.info("Read ACME directory.")
     directory = urlopen(config["acmednstiny"]["ACMEDirectory"])
-    acme_config = json.loads(directory.read().decode("utf8"))
+    acme_config = json.load(directory)
     if b"meta" in acme_config and b"terms-of-service" in acme_config["meta"]:
         terms_service_url = acme_config["meta"]["terms-of-service"]
     else:
@@ -198,10 +198,10 @@ def get_crt(config, log=LOGGER):
             while True:
                 try:
                     resp = urlopen(challenge["uri"])
-                    challenge_status = json.loads(resp.read().decode("utf8"))
+                    challenge_status = json.load(resp)
                 except IOError as e:
                     raise ValueError("Error checking challenge: {0} {1}".format(
-                        e.code, json.loads(e.read().decode("utf8"))))
+                        e.code, json.load(e)))
                 if challenge_status["status"] == "pending":
                     time.sleep(2)
                 elif challenge_status["status"] == "valid":
