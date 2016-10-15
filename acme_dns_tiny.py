a5af7fb6dbf39ce190729a880930667fcdee0a37
Adrien Dorsaz <adrien@adorsaz.ch>
2016-10-15 22:53:28 +0200

    Fixup registration update by improving HTTPError exception handling

--- acme_dns_tiny.py
+++ acme_dns_tiny.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-import argparse, subprocess, json, sys, base64, binascii, time, hashlib, re, copy, textwrap, logging
+import argparse, subprocess, json, sys, base64, binascii, time, hashlib, re, copy, textwrap, logging, urllib
 import dns.resolver, dns.tsigkeyring, dns.update
 from configparser import ConfigParser
-from urllib.request import urlopen
 
 LOGGER = logging.getLogger('acme_dns_tiny_logger')
 LOGGER.addHandler(logging.StreamHandler())
@@ -38,7 +37,7 @@ def get_crt(config, log=LOGGER):
     def _send_signed_request(url, payload):
         payload64 = _b64(json.dumps(payload).encode("utf8"))
         protected = copy.deepcopy(jws_header)
-        protected["nonce"] = urlopen(config["acmednstiny"]["ACMEDirectory"]).headers["Replay-Nonce"]
+        protected["nonce"] = urllib.urlopen(config["acmednstiny"]["ACMEDirectory"]).headers["Replay-Nonce"]
         protected64 = _b64(json.dumps(protected).encode("utf8"))
         signature = _openssl("dgst", ["-sha256", "-sign", config["acmednstiny"]["AccountKeyFile"]],
                              "{0}.{1}".format(protected64, payload64).encode("utf8"))
@@ -47,10 +46,10 @@ def get_crt(config, log=LOGGER):
             "payload": payload64, "signature": _b64(signature),
         })
         try:
-            resp = urlopen(url, data.encode("utf8"))
+            resp = urllib.urlopen(url, data.encode("utf8"))
             return resp.getcode(), resp.read(), resp.getheaders()
-        except IOError as e:
-            return getattr(e, "code", None), getattr(e, "read", e.__str__)(), None
+        except urllib.error.HTTPError as httperror:
+            return httperror.getcode(), httperror.read(), httperror.getheaders()
 
     # helper function to get url from Link HTTP headers
     def _get_url_link(headers, rel):
@@ -63,7 +62,7 @@ def get_crt(config, log=LOGGER):
     # main code
 
     log.info("Read ACME directory.")
-    directory = urlopen(config["acmednstiny"]["ACMEDirectory"])
+    directory = urllib.urlopen(config["acmednstiny"]["ACMEDirectory"])
     acme_config = json.loads(directory.read().decode("utf8"))
     directory_terms_url = acme_config.get("meta", {}).get("terms-of-service")
 
@@ -133,14 +132,12 @@ def get_crt(config, log=LOGGER):
         account_url = dict(headers).get("Location")
         log.info("Registered! (account: '{0}')".format(account_url))
     elif code == 409:
-        log.info("Already registered (headers: {0}, content: {1})".format(headers, result))
         account_url = dict(headers).get("Location")
-        if account_url is not None:
-            log.info("Get account informations (account: '{0}')".format(registration_url))
-            # Client should send empty payload to query account information
-            code, result, headers = _send_signed_request(account_url, {})
-            account_info = json.loads(result.read().decode("utf8"))
-            reg_received_terms = account_info.get("agreement")
+        log.info("Already registered! (account: '{0}')".format(account_url))
+        # Client should send empty payload to query account information
+        code, result, headers = _send_signed_request(account_url, {})
+        account_info = json.loads(result.decode("utf8"))
+        reg_received_terms = account_info.get("agreement")
     else:
         raise ValueError("Error registering: {0} {1} {2}".format(code, headers, result))
 
@@ -209,7 +206,7 @@ def get_crt(config, log=LOGGER):
         try:
             while True:
                 try:
-                    resp = urlopen(challenge["uri"])
+                    resp = urllib.urlopen(challenge["uri"])
                     challenge_status = json.loads(resp.read().decode("utf8"))
                 except IOError as e:
                     raise ValueError("Error checking challenge: {0} {1}".format(
@@ -236,7 +233,8 @@ def get_crt(config, log=LOGGER):
     certificate = "\n".join(textwrap.wrap(base64.b64encode(result).decode("utf8"), 64))
 
     # get the parent certificate which had created this one
-    resp = urlopen(_get_url_link(headers, 'up'))
+    certificate_parent_url = _get_url_link(headers, 'up')
+    resp = urllib.urlopen(certificate_parent_url)
     code = resp.getcode()
     result = resp.read()
     if code not in [200, 201]:
