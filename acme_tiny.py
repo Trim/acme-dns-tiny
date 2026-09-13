913f9b06d4745313245cd3facc6306ceb55c50ee
Adrien Dorsaz <adrien@adorsaz.ch>
2026-09-13 22:15:42 +0200

    challenge status can be `processing`, in that case continue polling
    The RFC 8555 defines in section `7.1.6 Status Changes` the valid status
    for challenge objects. A challenge object is created in a `pending`
    state. When the client announces the challenge is done, the server
    change the challenge status to `processing` and the client should
    continue to poll the challenge status until it reaches final states
    `invalid` or `valid`.
    
    Here is the relevant part of the section of the RFC 8555:
    
    ```
       Challenge objects are created in the "pending" state.  They
       transition to the "processing" state when the client responds to the
       challenge (see Section 7.5.1) and the server begins attempting to
       validate that the client has completed the challenge.  Note that
       within the "processing" state, the server may attempt to validate the
       challenge multiple times (see Section 8.2).  Likewise, client
       requests for retries do not cause a state change.  If validation is
       successful, the challenge moves to the "valid" state; if there is an
       error, the challenge moves to the "invalid" state.
    
                pending
                   |
                   | Receive
                   | response
                   V
               processing <-+
                   |   |    | Server retry or
                   |   |    | client retry request
                   |   +----+
                   |
                   |
       Successful  |   Failed
       validation  |   validation
         +---------+---------+
         |                   |
         V                   V
       valid              invalid
    
                      State Transitions for Challenge Objects
    ```

--- acme_tiny.py
+++ acme_tiny.py
@@ -148,7 +148,7 @@ def get_crt(account_key, csr, acme_dir, log=LOGGER, CA=DEFAULT_CA, disable_check
 
         # say the challenge is done
         _send_signed_request(challenge['url'], {}, "Error submitting challenges: {0}".format(domain))
-        authorization = _poll_until_not(auth_url, ["pending"], "Error checking challenge status for {0}".format(domain))
+        authorization = _poll_until_not(auth_url, ["pending", "processing"], "Error checking challenge status for {0}".format(domain))
         if authorization['status'] != "valid":
             raise ValueError("Challenge did not pass for {0}: {1}".format(domain, authorization))
         os.remove(wellknown_path)
